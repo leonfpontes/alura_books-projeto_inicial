7ce058f58a676ea4ea61a1b107d3a73496763258
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-04-16 15:29:11 -0300

    Consumindo API para mostrar livros

--- app/main.js
+++ app/main.js
@@ -1,18 +1,31 @@
 let livros = [];
 const requisicaoDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
+const elementoParaInserirLivros = document.getElementById('livros');
 
 async function getBuscarLivrosDaAPI(){
     try{
         const res = await fetch(requisicaoDaAPI);
         const livros = await res.json();
-        console.table(livros)
-
-        livros.forEach(livro => {
-            
-        });
+        exibirOsLivrosNaTela(livros);
 
     } catch (error){
         console.log(error);
     }
+}
+
+function exibirOsLivrosNaTela(listaDeLivros){
+    listaDeLivros.forEach(livro => {
+        elementoParaInserirLivros.innerHTML += `
+            <div class="livro">
+                <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+                <h2 class="livro__titulo">${livro.titulo}</h2>
+                <p class="livro__descricao">${livro.autor}</p>
+                <p class="livro__preco" id="preco">R$ ${livro.preco}</p>
+                <div class="tags">
+                    <span class="tag">${livro.categoria}</span>
+                </div>
+            </div>
+        `
+    })
 }
